393b610fffb064aa904c3519e20fae43becc01c3
Ciaran O Nuallain <onuallainc@gmail.com>
2020-09-02 16:33:26 +0100

    Creating elements

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-const title = React.Createelement
+const title = React.createElement(
+    'h1',
+    {id : 'main-title',title : 'This is a title'},
+    'Hello World'
+);
+
+const desc = React.createElement (
+    'p',
+    null,
+    'This is child p with empty props'
+);
+
+const header = React.createElement (
+    'header',
+    title,
+    desc
+);
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
