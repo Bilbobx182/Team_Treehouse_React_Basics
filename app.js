9a7e78416cde68d0f40f85bfcbca7a16b2c0cc5a
Ciaran O Nuallain <onuallainc@gmail.com>
2020-09-04 15:03:39 +0100

    Add scoreboard functionality

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 {/* React components upper case */}
-const Header = () =>
-     (
+const Header = () => {
+    return      (
         <header>
             <h1>Scoreboard</h1>
             <span className='stats'> Players : 1</span>
         </header>
     );
+}
 
 
+
+const Player = () => {
+    return (
+    <div className='player'>
+        <span className='player-name'>Ciaran</span>
+    
+        <div className='counter'>
+            <button className='counter-action decrement'> - </button>
+            <span className='counter-score'> </span>
+            <button className='counter-action increment'> + </button>
+        </div>
+    </div>
+    );
+}
+
+const Ciaran_App = () => {
+    return (<div>
+    <Header></Header>
+    <Player></Player>
+    </div>);
+
+}
 
 ReactDOM.render(
-    <Header />,
+    <Ciaran_App />,
     document.getElementById('root')
 );
